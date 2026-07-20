9deb303bc637c3593b10435a229f6c19324feee1
Sandro Richter <srichtersteinmetz@gmail.com>
2026-07-20 19:04:32 -0300

    Ajuste na pagamento server

--- src/util/PagamentoServer.java
+++ src/util/PagamentoServer.java
@@ -787,7 +787,7 @@ public class PagamentoServer {
                 String sql = "INSERT INTO notificacoes_pendentes " +
                         "(pedido_id, cod_peca, cliente, telefone, email, valor, frete, meio_pagamento, " +
                         "endereco, retirar_loja, itens, data_criacao, status, lida) " +
-                        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), 'PENDENTE', 0)";
+                        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), 'PENDENTE', 0)";
                 
                 stmt = con.prepareStatement(sql);
 
